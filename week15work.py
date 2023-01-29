436b095cf478167cca8b0b75091a1aa711e65177
Brenton Collins <brentcollins012@yahoo.com>
2023-01-29 19:16:43 +0000

    updated code

--- week15work.py
+++ week15work.py
@@ -1,9 +1,20 @@
 import boto3
 import logging
+import json
+from datetime import datetime
 
-sqs = boto3.client('sqs')  #<------SQS resource code
 
-sqs_queue = sqs.create_queue(QueueName='BlackSquad') #<----- Create the queue
+def lambda_handler(event, context):
 
-print(sqs_queue.url)
-print(sqs_queue)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S %p")
+
+    sqs = boto3.client('sqs')
+    sqs.send_message(
+        QueueUrl='https://sqs.us-east-1.amazonaws.com/358582585332/TheBlackSquad',
+        MessageBody=current_time
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(current_time)
+    }
